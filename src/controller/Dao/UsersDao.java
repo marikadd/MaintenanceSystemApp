3ed80985c8384f11b29ff9ffeacd90287fa8e5f5
dondi <dondi@192.168.1.7>
2020-11-25 15:23:48 +0100

    new users dao

---
+++ src/controller/Dao/UsersDao.java
@@ -0,0 +1,265 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.Dao;
+
+import configuration.Database.DBFactory;
+import configuration.Exceptions.InvalidParameterObjectException;
+import configuration.Exceptions.UnsuccessfulUpdateException;
+import configuration.Exceptions.UsernotFoundException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import model.Users.Maintainer;
+import model.Users.Planner;
+import model.Users.ProdManager;
+import model.Users.Role;
+import model.Users.SystemAdmin;
+import model.Users.UserModel;
+
+/**
+ *
+ * @author Group9
+ */
+public class UsersDao{
+    
+    private static UsersDao usersDao;
+    
+    public static UsersDao init() {
+        if(usersDao == null) usersDao = new UsersDao();
+        return usersDao;
+    }
+    
+    public UserModel findUserByUsername(String username, Role role) throws SQLException, UsernotFoundException {
+
+		Connection con = DBFactory.connectToDB();
+
+		String query = "select * from Users u " + "where u.User_Role = ? AND " + "u.Username = ?";
+
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, role.toString());
+		ps.setString(2, username);
+
+		ResultSet rs = ps.executeQuery();
+
+                UserModel userModel = null; 
+
+		if (rs.next()) {
+
+                    userModel = getSingleUserModel(rs, role);
+			
+
+		} else {
+			throw new UsernotFoundException("User with username " + username + " not found");
+		}
+
+		return userModel;
+
+	}
+    
+    public List<UserModel> findUsersByRole(Role role) throws SQLException, UsernotFoundException {
+        
+        Connection con = DBFactory.connectToDB();
+
+	String query = "select * from Users u " + "where u.User_Role = ?";
+
+	PreparedStatement ps = con.prepareStatement(query);
+	ps.setString(1, role.toString());
+        
+        ResultSet rs = ps.executeQuery();
+        List<UserModel> users = new LinkedList<UserModel>();
+        
+        while(rs.next()) {
+            
+            UserModel userModel = getSingleUserModel(rs, role);
+            users.add(userModel);
+            
+        }
+        
+        return users;
+        
+    }
+    
+    public void insertUserModel(UserModel userModel, Role role)
+            throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
+
+        validateUserModel(userModel);
+
+        Connection con = DBFactory.connectToDB();
+
+        String query = "INSERT INTO Users VALUES(?,?,?,?,?,?,?)";
+
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, userModel.getName());
+        ps.setString(2, userModel.getSurname());
+        ps.setString(3, userModel.getUsername());
+        ps.setString(4, userModel.getPassword());
+        ps.setString(5, userModel.getEmail());
+        ps.setString(6, userModel.getPhone());
+        ps.setString(7, role.toString());
+
+        boolean result = ps.execute();
+
+        if (!result) {
+            throw new UnsuccessfulUpdateException("Cannot create this user");
+        }
+    }
+
+    public void updateUserModel(String oldUsername, UserModel userModel)
+            throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
+
+        validateUserModel(userModel);
+
+        Connection con = DBFactory.connectToDB();
+
+        String query = "UPDATE Users SET User_Name = ?, Surname = ?, Username = ?, PW = ?, Email = ?, PhoneNumber = ? "
+                + "where Username = ?";
+
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, userModel.getName());
+        ps.setString(2, userModel.getSurname());
+        ps.setString(3, userModel.getUsername());
+        ps.setString(4, userModel.getPassword());
+        ps.setString(5, userModel.getEmail());
+        ps.setString(6, userModel.getPhone());
+        ps.setString(7, oldUsername);
+
+        boolean result = ps.execute();
+
+        if (!result) {
+            throw new UnsuccessfulUpdateException("Cannot update user with username " + oldUsername);
+        }
+
+    }
+
+    public void deleteUserModel(String username) throws SQLException, UnsuccessfulUpdateException {
+
+        Connection con = DBFactory.connectToDB();
+
+        String query = "DELETE FROM Users WHERE Username = ?";
+
+        PreparedStatement ps = con.prepareStatement(query);
+        ps.setString(1, username);
+
+        boolean result = ps.execute();
+
+        if (!result) {
+            throw new UnsuccessfulUpdateException("Cannot delete user with username " + username);
+        }
+
+    }
+    
+    private UserModel getSingleUserModel(ResultSet rs, Role role) throws SQLException, UsernotFoundException {
+        
+        UserModel userModel = null;
+        
+        switch(role) {
+                        case MAINTAINER: {
+                            userModel = new Maintainer(rs.getString("Username"), rs.getString("PW"), rs.getString("User_Name"),
+                                                    rs.getString("Surname"), rs.getString("Email"), rs.getString("PhoneNumber"));
+                            break;
+                        }
+                        case PROD_MANAGER : {
+                            userModel = new ProdManager(rs.getString("Username"), rs.getString("PW"), rs.getString("User_Name"),
+					rs.getString("Surname"), rs.getString("Email"), rs.getString("PhoneNumber"));
+                            break;
+                        }
+                        case PLANNER: {
+                            userModel = new Planner(rs.getString("Username"), rs.getString("PW"), rs.getString("User_Name"),
+                                          rs.getString("Surname"), rs.getString("Email"), rs.getString("PhoneNumber"));
+                            
+                            break;
+                        }
+                        case SYSTEM_ADMIN: {
+                            userModel = new SystemAdmin(rs.getString("Username"), rs.getString("PW"), rs.getString("User_Name"),
+                                         rs.getString("Surname"), rs.getString("Email"), rs.getString("PhoneNumber"));
+                            
+                            break;
+                        } 
+                        default: {
+                            throw new UsernotFoundException("User with role " + role.toString() + " not found");
+                        }
+                    }
+        
+        return userModel;
+        
+    }
+    
+    private void validateUserModel(UserModel userModel) throws InvalidParameterObjectException {
+
+        if (userModel == null) {
+            throw new InvalidParameterObjectException("The object parameters must be filled");
+        }
+
+        if (userModel.getUsername() == null) {
+            throw new InvalidParameterObjectException("User's username must be not null");
+        }
+
+        if (userModel.getUsername().length() > 20) {
+            throw new InvalidParameterObjectException("User's username must be at most 20 characters");
+        }
+
+        if (userModel.getName() == null) {
+            throw new InvalidParameterObjectException("User's name must be not null");
+        }
+
+        if (userModel.getName().length() > 20) {
+            throw new InvalidParameterObjectException("User's name must be at most 20 characters");
+        }
+
+        if (userModel.getSurname() == null) {
+            throw new InvalidParameterObjectException("User's surname must be not null");
+        }
+
+        if (userModel.getSurname().length() > 20) {
+            throw new InvalidParameterObjectException("User's surname must be at most 20 characters");
+        }
+
+        if (userModel.getPassword() == null) {
+            throw new InvalidParameterObjectException("User's password must be not null");
+        }
+
+        if (userModel.getPassword().length() > 50) {
+            throw new InvalidParameterObjectException("User's password must be at most 50 characters");
+        }
+
+        // Check valid password format
+        String passFormat = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,50}$";
+        if (!userModel.getPassword().matches(passFormat)) {
+            throw new InvalidParameterObjectException("Password must contains at least one number, one uppercase/lowercase letter and one special character. "
+                    + "White spaces are not allowed and password lenght must be at least 8 characters and at most 50");
+        }
+
+        if (userModel.getPhone() == null) {
+            throw new InvalidParameterObjectException("User's phone number must be not null");
+        }
+
+        if (userModel.getPhone().length() != 10) {
+            throw new InvalidParameterObjectException("User's phone number must be 10 characters");
+        }
+        
+        if(!userModel.getPhone().matches("^[0-9]$")) {
+            throw new InvalidParameterObjectException("User's phone number must be numeric");
+        }
+
+        if (userModel.getEmail() == null) {
+            throw new InvalidParameterObjectException("User's email must be not null");
+        }
+
+        if (userModel.getEmail().length() > 40) {
+            throw new InvalidParameterObjectException("User's email must be at most 40 characters");
+        }
+
+        // Check valid e-mail format (xxx@xxx.xxx)
+        String format = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
+        if (!userModel.getEmail().matches(format)) {
+            throw new InvalidParameterObjectException("Invalid e-mail address format");
+        }
+    }
+    
+}
--- src/controller/Services/UserManagementService.java
+++ src/controller/Services/UserManagementService.java
@@ -11,6 +11,8 @@ import model.Users.*;
 public class UserManagementService {
 
     private static UserManagementService ums;
+    
+    private UsersDao usersDao;
 
     // Singleton
     public static UserManagementService getUserManagementService() {
@@ -19,114 +21,98 @@ public class UserManagementService {
         }
         return ums;
     }
+    
+    public UserManagementService() {
+        usersDao = UsersDao.init();
+    }
 
-    public static ProdManager findProdManagerByUsername(String username) throws SQLException, UsernotFoundException {
+    public ProdManager findProdManagerByUsername(String username) throws SQLException, UsernotFoundException {
 
-        ProdManager manager = ProdManagerDao.findProdManagerByUsername(username);
-
+        ProdManager manager = (ProdManager) usersDao.findUserByUsername(username, Role.PROD_MANAGER);
         return manager;
     }
 
     public void insertProdManager(String username, String password, String name, String surname, String email,
             String phone) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        ProdManager prodManager = new ProdManager(username, password, name, surname, email, phone);
-        ProdManagerDao.insertProdManager(prodManager);
+        UserModel prodManager = new ProdManager(username, password, name, surname, email, phone);
+        usersDao.insertUserModel(prodManager, Role.PROD_MANAGER);
 
     }
 
-    public void deleteProdManager(String username) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
-        ProdManagerDao.deleteProdManager(username);
+    public void deleteUser(String username) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
+        usersDao.deleteUserModel(username);
     }
 
     public void updateProdManager(String oldUsername, String username, String password, String name, String surname, String email,
             String phone) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        ProdManager prodManager = new ProdManager(username, password, name, surname, email, phone);
-        ProdManagerDao.updateProdManager(oldUsername, prodManager);
+        UserModel userModel = new ProdManager(username, password, name, surname, email, phone);
+        usersDao.updateUserModel(oldUsername, userModel);
 
     }
+    
+    public SystemAdmin findSystemAdminByUsername(String username) throws SQLException, UsernotFoundException {
 
-    public static SystemAdmin findSystemAdminByUsername(String username) throws SQLException, UsernotFoundException {
-
-        SystemAdmin sysAdmin = SystemAdminDao.findSystemAdminByUsername(username);
-
+        SystemAdmin sysAdmin = (SystemAdmin) usersDao.findUserByUsername(username, Role.SYSTEM_ADMIN);
         return sysAdmin;
     }
 
     public void insertSystemAdmin(String username, String password, String name, String surname, String email,
             String phone) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        SystemAdmin sysAdmin = new SystemAdmin(username, password, name, surname, email, phone);
-        SystemAdminDao.insertSystemAdmin(sysAdmin);
+        UserModel sysAdmin = new SystemAdmin(username, password, name, surname, email, phone);
+        usersDao.insertUserModel(sysAdmin, Role.SYSTEM_ADMIN);
 
-    }
-
-    public void deleteSystemAdmin(String username) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
-        SystemAdminDao.deleteSystemAdmin(username);
     }
 
     public void updateSystemAdmin(String oldUsername, String username, String password, String name, String surname, String email,
             String phone) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        SystemAdmin sysAdmin = new SystemAdmin(username, password, name, surname, email, phone);
-        SystemAdminDao.updateSystemAdmin(oldUsername, sysAdmin);
+        UserModel userModel = new SystemAdmin(username, password, name, surname, email, phone);
+        usersDao.updateUserModel(oldUsername, userModel);
 
     }
     
-    public static LinkedList<Planner> findPlannerByUsername(String username) throws SQLException, UsernotFoundException {
+    public Planner findPlannerByUsername(String username) throws SQLException, UsernotFoundException {
 
-        LinkedList<Planner> listPlanner = new LinkedList<>();
-        
-        listPlanner = PlannerDao.findPlannerByUsername(username);
-
-        return listPlanner;
+        Planner planner = (Planner) usersDao.findUserByUsername(username, Role.PLANNER);
+        return planner;
     }
 
     public void insertPlanner(String username, String password, String name, String surname, String email, String phone) 
                               throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        Planner planner = new Planner(username, password, name, surname, email, phone);
-        PlannerDao.insertPlanner(planner);
-    }
-
-    public void deletePlanner(String username) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
-        PlannerDao.deletePlanner(username);
+        UserModel userModel = new Planner(username, password, name, surname, email, phone);
+        usersDao.insertUserModel(userModel, Role.PLANNER);
     }
 
     public void updatePlanner(String oldUsername, String username, String password, String name, String surname, String email, String phone) 
                               throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        Planner planner = new Planner(username, password, name, surname, email, phone);
-        PlannerDao.updatePlanner(oldUsername, planner);
+        UserModel userModel = new Planner(username, password, name, surname, email, phone);
+        usersDao.updateUserModel(oldUsername, userModel);
 
     }
     
-    public static LinkedList<Maintainer> findMaintainerByUsername(String username) throws SQLException, UsernotFoundException {
+    public Maintainer findMaintainerByUsername(String username) throws SQLException, UsernotFoundException {
 
-        LinkedList<Maintainer> listMaintainer = new LinkedList<>();
-        
-        listMaintainer = MaintainerDao.findMaintainerByUsername(username);
-
-        return listMaintainer;
+        Maintainer maintener = (Maintainer) usersDao.findUserByUsername(username, Role.MAINTAINER);
+        return maintener;
     }
 
     public void insertMaintainer(String username, String password, String name, String surname, String email, String phone) 
                               throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        Maintainer maintainer = new Maintainer(username, password, name, surname, email, phone);
-        MaintainerDao.insertMaintainer(maintainer);
-    }
-
-    public void deleteMaintainer(String username) throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
-        MaintainerDao.deleteMaintainer(username);
+        UserModel userModel = new Maintainer(username, password, name, surname, email, phone);
+        usersDao.insertUserModel(userModel, Role.MAINTAINER);
     }
 
     public void updateMaintainer(String oldUsername, String username, String password, String name, String surname, String email, String phone) 
                               throws InvalidParameterObjectException, SQLException, UnsuccessfulUpdateException {
 
-        Maintainer maintainer = new Maintainer(username, password, name, surname, email, phone);
-        MaintainerDao.updateMaintainer(oldUsername, maintainer);
+        UserModel userModel = new Maintainer(username, password, name, surname, email, phone);
+        usersDao.updateUserModel(oldUsername, userModel);
 
     }
     
