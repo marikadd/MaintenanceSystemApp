27d2ab857268626d0768c2cf939ff72a52d24998
dondi <dondi@192.168.1.7>
2020-11-25 09:50:05 +0100

    competence dao insert

---
+++ src/configuration/Exceptions/CompetencenotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package configuration.Exceptions;
+
+/**
+ *
+ * @author dondi
+ */
+public class CompetencenotFoundException extends Exception {
+
+    public CompetencenotFoundException(String message) {
+        super(message);
+    }
+
+}
