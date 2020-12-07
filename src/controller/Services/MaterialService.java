687dca060407355571d46c991d7096c669d1b367
giusidp <36866227+giusidp@users.noreply.github.com>
2020-12-07 16:52:36 +0100

    new classes for materials (model, dao, service)

---
+++ src/controller/Services/MaterialService.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.Services;
+
+import configuration.Exceptions.InvalidParameterObjectException;
+import configuration.Exceptions.InvalidPermissionException;
+import configuration.Exceptions.UnsuccessfulUpdateException;
+import controller.Dao.MaterialDao;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import model.Department.Department;
+import model.Material.Material;
+
+/**
+ *
+ * @author Group9
+ */
+public class MaterialService {
+        private static MaterialService matService;
+        private MaterialDao matDao;
+        
+        //Singleton
+         public static MaterialService getDepartmentService() {
+        if (matService == null) {
+            matService = new MaterialService();
+            matService.matDao = MaterialDao.init();
+        }
+        return matService;
+    }
+         
+        public int insertMaterial(String type)
+            throws InvalidPermissionException, SQLException, UnsuccessfulUpdateException, InvalidParameterObjectException {
+
+            return matDao.insertMaterial(type);
+        }
+        
+        public int updateMaterial(String oldType, String newType) throws SQLException, UnsuccessfulUpdateException, InvalidParameterObjectException {
+
+            return matDao.updateMaterial(oldType, newType);
+        }
+
+        public int deleteDepartment(String type) throws SQLException, UnsuccessfulUpdateException {
+
+            return matDao.deleteMaterial(type);
+        }
+        
+        public List<Material> getAllMaterials() throws SQLException {
+
+            List<Material> matList = new LinkedList<>();
+            matList = matDao.findAllMaterials();
+
+            return matList;
+        }       
+}
